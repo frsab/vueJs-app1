b136de8ee0c17ad7eb7d0de29c39e8c1a5e5ca52
Saber FRAJ <frejsaber@gmail.com>
2019-02-21 19:00:20 +0100

    add

--- src/components/recherche/recherche.vue
+++ src/components/recherche/recherche.vue
@@ -2,8 +2,17 @@
 
   <section class="recherche">
     <h1>recherche Component</h1>
+    <div class="home">
+      <ul class="posts">
+        <li v-for="post in posts.listeResultat" :key="post.nom" class="post-item">
+          <h1>{{ post.nom }}</h1>
+          <h1>{{ post.prenom }}</h1>
+          <h1>{{ post.taille }}</h1>
+          <h1>{{ post.enCouple }}</h1>
+        </li>
+      </ul>
+    </div>
   </section>
-
 </template>
 
 <script lang="js">
@@ -22,13 +31,58 @@
 
     },
     computed: {
-
+      /**
+       *
+        * @returns {*}
+       */
+      posts () {
+        return {
+          "listeCritere": [
+            "nom",
+            "prenom",
+            "taille",
+            "enCouple"
+          ],
+          "listeResultat": [
+            {
+              "nom": "fraj",
+              "prenom": "saber",
+              "taille": 180,
+              "enCouple": false
+            },
+            {
+              "nom": "fraj",
+              "prenom": "ALbert",
+              "taille": 120,
+              "enCouple": true
+            },
+            {
+              "nom": "Dubois",
+              "prenom": "martin",
+              "taille": 177,
+              "enCouple": false
+            },
+            {
+              "nom": "Hollande",
+              "prenom": "François",
+              "taille": 180,
+              "enCouple": false
+            }
+          ]
+        }
+        //this.$store.state.posts
+      }
     }
 }
 </script>
 
-<style scoped lang="scss">
-  .recherche {
+<style scoped>
+  .posts {
+    list-style: none;
+    text-align: left;
+  }
 
+  .post-item + .post-item {
+    border-top: 1px solid rgba(0, 0, 0, 0.1);
   }
 </style>
