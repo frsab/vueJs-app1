16a9b3ced302da75ca282cbd0d368e6fd644b51c
Saber FRAJ <frejsaber@gmail.com>
2019-02-21 21:40:31 +0100

    binding

---
+++ src/components/myComponent.vue
@@ -0,0 +1,40 @@
+<template lang="html">
+
+  <section class="my-component">
+    <h1>my-component Component</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+  export default  {
+    name: 'MyComponent',
+    props: [
+            'prop1'
+    ],
+    mounted() {
+
+    },
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+      fireEvent() {
+        this.$emit('myEvent', eventValueOne, eventValueTwo);
+      }
+
+    },
+    computed: {
+
+    }
+}
+</script>
+
+<style scoped lang="scss">
+  .my-component {
+
+  }
+</style>
